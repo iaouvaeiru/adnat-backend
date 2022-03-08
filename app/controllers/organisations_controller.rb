57f81ea0cc6d6272767ffabf89ce02787a5f47a1
Leon Fan <leonkind@gmail.com>
2022-03-07 19:38:00 -0600

    added create organisation and auto joining functionality

--- app/controllers/organisations_controller.rb
+++ app/controllers/organisations_controller.rb
@@ -1,4 +1,5 @@
 class OrganisationsController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: :render_error
     def index 
         organisations = Organisation.all
         render json: organisations
@@ -8,5 +9,22 @@ class OrganisationsController < ApplicationController
         organisation = Organisation.find_by(id: params[:id])
         organisation.update(:name => params[:name], :hourly_rate => params[:hourly_rate])
         render json: organisation, status: 200
+    end
+
+    def create
+        organisation = Organisation.create!(organisation_params)
+        render json: organisation, status: 201
+        user = User.find_by(id: params[:user_id])
+        user.update(:organisation_id => organisation.id)
+    end
+
+    private
+
+    def organisation_params
+        params.permit(:name, :hourly_rate)
+    end
+
+    def render_error(invalid)
+        render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
     end
 end
