7116a3e31a95b4b1d08b12bbf12b8eeb0307cea9
Leon Fan <leonkind@gmail.com>
2022-03-07 21:40:10 -0600

    additional seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Organisation.create({name: "Bob's Burgers", hourly_rate: 10})
-Organisation.create({name: "Moe's Tavern", hourly_rate: 10})
-Organisation.create({name: "Sally's Sandwiches", hourly_rate: 10})
+Organisation.create({name: "Super Tommy's Dinner Roll Wholesale", hourly_rate: 14})
+Organisation.create({name: "Crazy Johnnie's Artisanal Sporks", hourly_rate: 23})
+Organisation.create({name: "Jim's Gym", hourly_rate: 10})
+
+User.create({name: "Timothy McJimothy Esquire the 2nd, Holy Emperor of the Roman Empire", email_address: "timmcjim321@tim.com", password: "bababa123"})
+User.create({name: "Tricky Ricky", email_address: "teerick@aol.com", password: "ttrickums44321", organisation_id: 2})
+User.create({name: "Hannah Montana", email_address: "mileycyrus@disney.com", password: "abc123"})
+
+Shift.create({start: "2022-03-04 09:15:00", finish: "2022-03-04 18:00:00", break_length: 45, user_id: 2})
+Shift.create({start: "2022-03-05 09:00:00", finish: "2022-03-05 17:00:00", break_length: 30, user_id: 2})
+Shift.create({start: "2022-03-06 12:45:00", finish: "2022-03-06 19:30:00", break_length: 60, user_id: 2})
