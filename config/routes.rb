c9f4981f6a06c8e2e18ec30892edc1a19cf26587
Leon Fan <leonkind@gmail.com>
2022-03-08 21:19:08 -0600

    added jwt gem functions

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   patch "/leave", to: 'users#leave'
   patch "/edit", to: 'users#edit'
+  post "/login", to: 'users#login'
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
