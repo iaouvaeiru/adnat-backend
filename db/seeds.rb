6602675d8b2c45594eef8b3714ddad2f21db0eca
Leon Fan <leonkind@gmail.com>
2022-03-09 16:31:27 -0600

    added more seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,11 +9,17 @@
 Organisation.create({name: "Super Tommy's Dinner Roll Wholesale", hourly_rate: 14})
 Organisation.create({name: "Crazy Johnnie's Artisanal Sporks", hourly_rate: 23})
 Organisation.create({name: "Jim's Gym", hourly_rate: 10})
+Organisation.create({name: 'Cool Kids Club', hourly_rate: 22})
+Organisation.create({name: 'Sumptuous Seedful Watermelons Shoppe', hourly_rate: 37})
 
-User.create({name: "Timothy McJimothy Esquire the 2nd, Holy Emperor of the Roman Empire", email_address: "timmcjim321@tim.com", password: "bababa123"})
+User.create({name: "Timothy McJimothy Esquire the 2nd, Holy Emperor of the Roman Empire", email_address: "timmcjim321@tim.com", password: "bababa123", organisation_id: 3})
 User.create({name: "Tricky Ricky", email_address: "teerick@aol.com", password: "ttrickums44321", organisation_id: 2})
 User.create({name: "Hannah Montana", email_address: "mileycyrus@disney.com", password: "abc123"})
+User.create({name: 'Baby Whale', email_address: "whales@ocean.com", password: "whales", organisation_id:2})
+User.create({name: 'Non Conformist', email_address: "noway@hotmail.com", password: "nonconformist"})
 
 Shift.create({start: "2022-03-04 09:15:00", finish: "2022-03-04 18:00:00", break_length: 45, user_id: 2})
 Shift.create({start: "2022-03-05 09:00:00", finish: "2022-03-05 17:00:00", break_length: 30, user_id: 2})
 Shift.create({start: "2022-03-06 12:45:00", finish: "2022-03-06 19:30:00", break_length: 60, user_id: 2})
+Shift.create({start: "2022-03-06 11:45:00", finish: "2022-03-06 22:00:00", break_length: 60, user_id: 4})
+Shift.create({start: "2022-03-10 09:15:00", finish: "2022-03-10 19:30:00", break_length: 30, user_id:1})
