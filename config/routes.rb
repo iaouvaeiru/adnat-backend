e6c0707e4d5f0ab2820fad3a4487775def94ae29
Leon Fan <leonkind@gmail.com>
2022-03-15 14:58:57 -0500

    set up password reset message

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
 
   get "/reset", to: 'password_resets#new'
   post "/reset", to: 'password_resets#create'
+  get "/reset/edit", to: 'password_resets#edit'
+  patch "/reset/edit", to: 'password_resets#update'
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
