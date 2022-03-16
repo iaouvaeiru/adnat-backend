2277e54967b0ae6855ea24b42ae23b46f26e2080
Leon Fan <leonkind@gmail.com>
2022-03-15 19:00:44 -0500

    requires unique email and password to create user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   belongs_to :organisation
   has_many :shifts
   has_secure_password
+
+  validates :username, uniqueness: {case_sensitive: false}
+  validates :password, presence: true
 end
